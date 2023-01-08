3852902c237b6e216b4228d5ccc34b079597820e
M Zaenal Mustofa <zendzzo@gmail.com>
2023-01-08 14:58:40 +0700

    inital commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addNoteHandlers,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require("./handler");
+
+const routes = [
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNoteHandlers,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
